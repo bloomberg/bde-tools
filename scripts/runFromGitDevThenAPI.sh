319567515650a10d6c5921845d098357167af496
Mike Giroux <mgiroux@bloomberg.net>
2011-06-14 15:52:22 -0400

    move logic for updating repos, calling buildSnapshot, then deploying snapshots into this script (from buildSnapshot and bldmgr)

--- scripts/runFromGitDevThenAPI.sh
+++ scripts/runFromGitDevThenAPI.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/bash
 
+TOOLSPATH=/home/bdebuild/bde-tools
 SCRIPT_NAME=runFromGitDevThenAPI
 VIEW_NAME=bde_devintegrator
-GIT_REPO=~/bs/git-bde-dev
+GIT_REPO=/home/bdebuild/bs/git-bde-dev
+BUILD_DIR=/home/bdebuild/bs/build-dev
 
-/usr/atria/bin/cleartool startview bde_devintegrator
+W32_BUILD_DIR=bdenydev01:/e/nightly_builds/dev
+W32_BUILD_DIR=apinydev01:/d/nightly_builds/dev
 
-cd $GIT_REPO 2> /dev/null
-git pull
+SNAPSHOT_DIR=/home/bdebuild/bs/snapshot-dev
+TARBALL=/home/bdebuild/bs/tars-dev/snapshot-dev.`date +"%Y%m%d"`.tar.gz
+DEV_UORS="bsl zde bde bbe bce bae bte bsi                                   \
+       a_bdema a_bteso a_xercesc bsc e_ipc a_ossl a_fsipc bas a_xmf         \
+       a_baslt a_bassvc bap a_comdb2 a_basfs a_bascat z_bae a_fsbaem z_bas"
+API_UORS="api apt apu aps apn blpapi"
+FDE_UORS="fde"
 
 # redirect outputs so we can track failures - nysbldo2 does
 # not mail cron job results
 exec > ~bdebuild/logs/log.$SCRIPT_NAME.`date +"%Y%m%d-%H%M%S"` 2>&1
+
+/usr/atria/bin/cleartool startview $VIEW_NAME
+
+cd $GIT_REPO 2> /dev/null
+git pull
 
 SCRIPT_PATH=$(dirname $0)
 
@@ -20,40 +33,49 @@ then \
     SCRIPT_PATH="$SCRIPT_PATH/"
 fi
 
-PATH="/opt/SUNWspro/bin/:/bbcm/infrastructure/tools/bin:/usr/bin:/usr/sbin:/sbin:/usr/bin/X11:/usr/local/bin:/bb/bin:/bb/shared/bin:/bb/shared/abin:/bb/bin/robo:/bbsrc/tools/bbcm:/bbsrc/tools/bbcm/parent:/usr/atria/bin"
+PATH="/home/bdebuild/bde-tools/bin:/home/bdebuild/bde-tools/scripts:/opt/SUNWspro/bin/:/bbcm/infrastructure/tools/bin:/usr/bin:/usr/sbin:/sbin:/usr/bin/X11:/usr/local/bin:/bb/bin:/bb/shared/bin:/bb/shared/abin:/bb/bin/robo:/bbsrc/tools/bbcm:/bbsrc/tools/bbcm/parent:/usr/atria/bin"
 export PATH
 
+buildSnapshot.sh $TARBALL $OUTPUTPATH $GIT_REPO /view/$VIEW_NAME/bbcm/{infrastructure,api} \
+                 -- \
+                 $DEV_UORS $API_UORS $FDE_UORS
+
+cd $BUILD_DIR
+echo synchronizing $OUTPUTPATH and $BUILD_DIR
+# add --delete switch to make sure deleted files are reflected
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $BUILD_DIR/
+
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W32_BUILD_DIR/
+
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W64_BUILD_DIR/
+
 # run dev build
-/view/bde_devintegrator/bbcm/infrastructure/tools/bin/bde_bldmgr -v                \
-       -k /view/bde_devintegrator/bbcm/infrastructure/tools/etc/bde_bldmgr.config  \
+$TOOLSPATH/bin/bde_bldmgr -v                \
+       -k $TOOLSPATH/etc/bde_bldmgr.config  \
        -f -k -m -idev                                                              \
-       bsl zde bde bbe bce bae bte                                                 \
-       bsi                                                                         \
-       a_bdema a_bteso a_xercesc bsc e_ipc a_ossl a_fsipc bas a_xmf         \
-       a_baslt a_bassvc bap a_comdb2 a_basfs a_bascat z_bae a_fsbaem z_bas   \
+       $DEV_UORS  \
        < /dev/null 2>&1                                      \
    | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev \
    && /home/bdebuild/bin/report-latest dev
 
 # THEN run api and fde builds
-/view/bde_devintegrator/bbcm/infrastructure/tools/bin/bde_bldmgr -v                \
-        -k /view/bde_devintegrator/bbcm/infrastructure/tools/etc/bde_bldmgr.config \
+$TOOLSPATH/bin/bde_bldmgr -v                \
+        -k $TOOLSPATH/etc/bde_bldmgr.config \
         -f -k -m -idev-api -wbde_devintegrator                                     \
-        api apt apu aps apn blpapi                                                 \
+        $API_UORS                                                \
         < /dev/null 2>&1                                                \
    | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev-api   \
    && /home/bdebuild/bin/report-latest dev-api &
 
-/view/bde_devintegrator/bbcm/infrastructure/tools/bin/bde_bldmgr -v                \
-        -k /view/bde_devintegrator/bbcm/infrastructure/tools/etc/bde_bldmgr.config \
+$TOOLSPATH/bin/bde_bldmgr -v                \
+        -k $TOOLSPATH/etc/bde_bldmgr.config \
         -f -k -m -idev-fde -wbde_devintegrator                                     \
-        fde                                                                        \
+        $FDE_UORS                                                                        \
         < /dev/null 2>&1                                              \
   | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev-fde       \
   && /home/bdebuild/bin/report-latest dev-fde &
 
 wait
-
 
 ~bdebuild/bin/generateGccWarningsLogs.pl dev bde_devintegrator
 
