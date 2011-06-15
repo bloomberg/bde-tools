628ffe94917444ff9cfe99eb8d56fdbbf5c55cbb
Mike Giroux <mgiroux@bloomberg.net>
2011-06-15 12:14:52 -0400

    ready to test

--- scripts/runFromGitDevThenAPI.sh
+++ scripts/runFromGitDevThenAPI.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-TOOLSPATH=/home/bdebuild/bde-tools
 SCRIPT_NAME=runFromGitDevThenAPI
+BUILD_TYPE=dev
+
+TOOLSPATH=/home/bdebuild/bde-tools
 VIEW_NAME=bde_devintegrator
-GIT_REPO=/home/bdebuild/bs/git-bde-dev
-BUILD_DIR=/home/bdebuild/bs/build-dev
+GIT_REPO=/home/bdebuild/bs/git-bde-${BUILD_TYPE}
+BUILD_DIR=/home/bdebuild/bs/build-${BUILD_TYPE}
 
-W32_BUILD_DIR=bdenydev01:/e/nightly_builds/dev
-W32_BUILD_DIR=apinydev01:/d/nightly_builds/dev
+W32_BUILD_DIR=bdenydev01:/e/nightly_builds/${BUILD_TYPE}
+W32_BUILD_DIR=apinydev01:/d/nightly_builds/${BUILD_TYPE}
 
-SNAPSHOT_DIR=/home/bdebuild/bs/snapshot-dev
-TARBALL=/home/bdebuild/bs/tars-dev/snapshot-dev.`date +"%Y%m%d"`.tar.gz
+SNAPSHOT_DIR=/home/bdebuild/bs/snapshot-${BUILD_TYPE}
+TARBALL=/home/bdebuild/bs/tars-${BUILD_TYPE}/snapshot-${BUILD_TYPE}.`date +"%Y%m%d"`.tar.gz
 DEV_UORS="bsl zde bde bbe bce bae bte bsi                                   \
        a_bdema a_bteso a_xercesc bsc e_ipc a_ossl a_fsipc bas a_xmf         \
        a_baslt a_bassvc bap a_comdb2 a_basfs a_bascat z_bae a_fsbaem z_bas"
@@ -21,61 +23,59 @@ FDE_UORS="fde"
 # not mail cron job results
 exec > ~bdebuild/logs/log.$SCRIPT_NAME.`date +"%Y%m%d-%H%M%S"` 2>&1
 
-/usr/atria/bin/cleartool startview $VIEW_NAME
-
-cd $GIT_REPO 2> /dev/null
-git pull
-
-SCRIPT_PATH=$(dirname $0)
-
-if [ ! -z "$SCRIPT_PATH" ]
-then \
-    SCRIPT_PATH="$SCRIPT_PATH/"
-fi
-
-PATH="/home/bdebuild/bde-tools/bin:/home/bdebuild/bde-tools/scripts:/opt/SUNWspro/bin/:/bbcm/infrastructure/tools/bin:/usr/bin:/usr/sbin:/sbin:/usr/bin/X11:/usr/local/bin:/bb/bin:/bb/shared/bin:/bb/shared/abin:/bb/bin/robo:/bbsrc/tools/bbcm:/bbsrc/tools/bbcm/parent:/usr/atria/bin"
+PATH="$TOOLSPATH/bin:$TOOLSPATH/scripts:/opt/SUNWspro/bin/:/usr/bin:/usr/sbin:/sbin:/usr/bin/X11:/usr/local/bin:/bb/bin:/bb/shared/bin:/bb/shared/abin:/bb/bin/robo:/bbsrc/tools/bbcm:/bbsrc/tools/bbcm/parent:/usr/atria/bin"
 export PATH
 
-buildSnapshot.sh $TARBALL $OUTPUTPATH $GIT_REPO /view/$VIEW_NAME/bbcm/{infrastructure,api} \
+/usr/atria/bin/cleartool startview $VIEW_NAME
+
+pushd $GIT_REPO 2> /dev/null
+/opt/swt/bin/git pull
+popd
+
+SCRIPT_PATH=$TOOLSPATH/scripts
+
+$SCRIPT_PATH/buildSnapshot.sh $TARBALL $OUTPUTPATH $GIT_REPO /view/$VIEW_NAME/bbcm/{infrastructure,api} \
                  -- \
                  $DEV_UORS $API_UORS $FDE_UORS
 
 cd $BUILD_DIR
 echo synchronizing $OUTPUTPATH and $BUILD_DIR
 # add --delete switch to make sure deleted files are reflected
-/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $BUILD_DIR/
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $BUILD_DIR/ 2>&1 | perl -pe's/^/UNIX-CP: /' &
 
-/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W32_BUILD_DIR/
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W32_BUILD_DIR/ 2>&1 | perl -pe's/^/W32-CP: /' &
 
-/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W64_BUILD_DIR/
+/opt/swt/bin/rsync -av --delete $OUTPUTPATH/ $W64_BUILD_DIR/ 2>&1 | perl -pe's/^/W64-CP: /' &
+
+wait
 
 # run dev build
 $TOOLSPATH/bin/bde_bldmgr -v                \
        -k $TOOLSPATH/etc/bde_bldmgr.config  \
-       -f -k -m -idev                                                              \
-       $DEV_UORS  \
-       < /dev/null 2>&1                                      \
-   | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev \
-   && /home/bdebuild/bin/report-latest dev
+       -f -k -m -i${BUILD_TYPE}             \
+       $DEV_UORS                            \
+       < /dev/null 2>&1                     \
+   | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.${BUILD_TYPE} \
+   && /home/bdebuild/bin/report-latest ${BUILD_TYPE}
 
 # THEN run api and fde builds
 $TOOLSPATH/bin/bde_bldmgr -v                \
         -k $TOOLSPATH/etc/bde_bldmgr.config \
-        -f -k -m -idev-api -wbde_devintegrator                                     \
-        $API_UORS                                                \
-        < /dev/null 2>&1                                                \
-   | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev-api   \
-   && /home/bdebuild/bin/report-latest dev-api &
+        -f -k -m -i${BUILD_TYPE}-api        \
+        $API_UORS                           \
+        < /dev/null 2>&1                    \
+   | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.${BUILD_TYPE}-api   \
+   && /home/bdebuild/bin/report-latest ${BUILD_TYPE}-api &
 
 $TOOLSPATH/bin/bde_bldmgr -v                \
         -k $TOOLSPATH/etc/bde_bldmgr.config \
-        -f -k -m -idev-fde -wbde_devintegrator                                     \
-        $FDE_UORS                                                                        \
-        < /dev/null 2>&1                                              \
-  | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.dev-fde       \
-  && /home/bdebuild/bin/report-latest dev-fde &
+        -f -k -m -i${BUILD_TYPE}-fde        \
+        $FDE_UORS                           \
+        < /dev/null 2>&1                    \
+  | /home/bdebuild/bin/logTs.pl /home/bdebuild/logs/log.${BUILD_TYPE}-fde       \
+  && /home/bdebuild/bin/report-latest ${BUILD_TYPE}-fde &
 
 wait
 
-~bdebuild/bin/generateGccWarningsLogs.pl dev bde_devintegrator
+~bdebuild/bin/generateGccWarningsLogs.pl ${BUILD_TYPE} bde_devintegrator
 
