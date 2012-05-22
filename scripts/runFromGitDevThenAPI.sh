fc3a10b4ff248a781422e83a662b3d6831b962a4
Mike Giroux <mgiroux@bloomberg.net>
2012-05-22 13:56:46 -0400

    delete all api builds

--- scripts/runFromGitDevThenAPI.sh
+++ scripts/runFromGitDevThenAPI.sh
@@ -12,9 +12,6 @@ BDE_BB_GIT_REPO=/home/bdebuild/bs/bde-bb-${BUILD_TYPE}
 
 BAS_GIT_REPO=/home/bdebuild/bs/bas-libs-${BUILD_TYPE}
 
-# Actual api repo is $API_GIT_REPO/groups
-API_GIT_REPO=/home/bdebuild/bs/api-libs-${BUILD_TYPE}
-
 BUILD_DIR=/home/bdebuild/bs/build-${BUILD_TYPE}
 LOG_DIR=/home/bdebuild/bs/nightly-logs/${BUILD_TYPE}
 
@@ -31,12 +28,8 @@ DEV_UORS="bsl zde bde bbe bce bae bte bsi                                   \
        a_bdema a_bteso a_xercesc bsc e_ipc a_ossl a_fsipc bas a_xmf         \
        a_baslt bap a_bassvc a_comdb2 a_basfs a_bascat z_bae a_fsbaem z_bas  \
        z_a_bdema"
-API_UORS="api apt apu aps apn blpapi"
-FDE_UORS="fde"
 
 #DEV_UORS=bsl
-#API_UORS=
-#FDE_UORS=
 
 export BUILD_DIR LOG_DIR TOOLSPATH
 
@@ -64,23 +57,11 @@ pushd $BAS_GIT_REPO 2> /dev/null
 /opt/swt/bin/git checkout remotes/origin/proposed-updates
 popd
 
-pushd $API_GIT_REPO/groups 2> /dev/null
-/opt/swt/bin/git fetch
-/opt/swt/bin/git checkout remotes/origin/master
-popd
-
 SCRIPT_PATH=$TOOLSPATH/scripts
 
 $SCRIPT_PATH/buildSnapshot.sh $TARBALL $SNAPSHOT_DIR $BDE_CORE_GIT_REPO $BDE_BB_GIT_REPO $BAS_GIT_REPO /view/$VIEW_NAME/bbcm/{infrastructure,api} \
                  -- \
-                 $DEV_UORS $API_UORS $FDE_UORS
-
-# bde_snapshot.pl can fail if a group has bad metadata or header errors.
-# This is a particularly a problem with API libs.
-for grp in ${API_UORS/ blpapi/}; do \
-    rsync -av --exclude='unix-*' --exclude='windows-*' \
-        /view/$VIEW_NAME/bbcm/api/groups/$grp/ $SNAPSHOT_DIR/groups/$grp/
-done
+                 $DEV_UORS
 
 cd $BUILD_DIR
 echo synchronizing $OUTPUTPATH and $BUILD_DIR
@@ -120,25 +101,4 @@ $TOOLSPATH/bin/bde_bldmgr -v                \
 
 # generate gcc warnings
 $TOOLSPATH/scripts/generateGccWarningsLogs.pl ${BUILD_TYPE} ${LOG_DIR}
-
-# THEN run api and fde builds - set deadline for these to 23:30
-$TOOLSPATH/bin/bde_bldmgr -v                \
-        -k $TOOLSPATH/etc/bde_bldmgr.config \
-        -f -k -m -i${BUILD_TYPE}-api        \
-        -T 23:30                            \
-        $API_UORS                           \
-        < /dev/null 2>&1                    \
-   | $TOOLSPATH/scripts/logTs.pl /home/bdebuild/logs/log.${BUILD_TYPE}-api   \
-   && $TOOLSPATH/scripts/report-latest ${BUILD_TYPE}-api &
-
-$TOOLSPATH/bin/bde_bldmgr -v                \
-        -k $TOOLSPATH/etc/bde_bldmgr.config \
-        -f -k -m -i${BUILD_TYPE}-fde        \
-        -T 23:30                            \
-        $FDE_UORS                           \
-        < /dev/null 2>&1                    \
-  | $TOOLSPATH/scripts/logTs.pl /home/bdebuild/logs/log.${BUILD_TYPE}-fde       \
-  && $TOOLSPATH/scripts/report-latest ${BUILD_TYPE}-fde &
-
-wait
 
