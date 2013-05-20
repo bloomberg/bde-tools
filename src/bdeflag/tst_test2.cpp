1afc3f1d618f30e5d86841549c6db2e154a73306
Bill Chapman <bchapman2@bloomberg.net>
2013-05-20 11:17:00 -0400

    Add managed ptr and shared ptr to RAII detection

--- src/bdeflag/tst_test2.cpp
+++ src/bdeflag/tst_test2.cpp
@@ -32,5 +32,29 @@ void woof()
 
         bcemt_LockGuard&  guard;
         bcemt::LockGuard& guard;
+
+        bdema_ManagedPtr<Woof> woofPtr(&w);
+        bdema::ManagedPtr<Woof> woofPtr(&w);
+
+        bdema_ManagedPtr<Woof>();    // 39
+        bdema::ManagedPtr<Woof>();   // 40
+
+        bdema_ManagedPtr<Woof>  guard;
+        bdema::ManagedPtr<Woof> guard;
+
+        bdema_ManagedPtr();    // 45
+        bdema::ManagedPtr();   // 46
+
+        bcema_SharedPtr<Woof>  woofPtr(&w);
+        bcema::SharedPtr<Woof> woofPtr(&w);
+
+        bcema_SharedPtr<Woof>(&w);    // 51
+        bcema::SharedPtr<Woof>(&w);   // 52
+
+        bcema_SharedPtr<Woof>  woofPtr;
+        bcema::SharedPtr<Woof> woofPtr;
+
+        bcema_SharedPtr();    // 57
+        bcema::SharedPtr();   // 58
     }
 }
