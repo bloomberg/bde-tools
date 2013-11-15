5a9e31c089f99dc25e3f7d3344f63e7ca06b1095
Mike Giroux <mgiroux@bloomberg.net>
2013-11-15 14:20:25 -0500

    add a_iconv to dev nightly build

--- scripts/nightly-run-dev.sh
+++ scripts/nightly-run-dev.sh
@@ -11,7 +11,7 @@ BDE_BB_BRANCH=remotes/origin/dev-integration
 BSL_TYPE=internal
 
 CORE_UORS="bsl bst bde bbe bce bae bte"
-BB_UORS="bdx bsi zde a_bdema a_bteso a_xercesc e_ipc z_a_bdema bap a_comdb2 a_cdrdb z_bae a_cdrcache"
+BB_UORS="bdx bsi zde a_bdema a_bteso a_xercesc e_ipc z_a_bdema bap a_comdb2 a_cdrdb z_bae a_cdrcache a_iconv"
 
 ALL_UORS="$CORE_UORS $BB_UORS"
 
