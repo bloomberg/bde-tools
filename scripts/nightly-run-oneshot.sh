5180cfffd919cdc6d56710adfdaa9533fdb5a1d3
Mike Giroux <mgiroux@bloomberg.net>
2015-03-24 10:17:42 -0400

    make oneshot better

--- scripts/nightly-run-oneshot.sh
+++ scripts/nightly-run-oneshot.sh
@@ -10,10 +10,20 @@ BDE_BB_BRANCH=remotes/origin/master
 
 BSL_TYPE=internal
 
-CORE_UORS="bsl"
-BB_UORS=""
+if false
+then
+    # just bsl
+    CORE_UORS="bsl"
+    BB_UORS=""
+else
+    # full set
+    CORE_UORS="bsl bdl bde bbe bce bae bte"
+    BB_UORS="bdx bsi zde a_bdema a_bteso a_xercesc e_ipc z_a_bdema bap a_comdb2 a_cdrdb z_bae a_cdrcache a_iconv"
+fi
 
 ALL_UORS="$CORE_UORS $BB_UORS"
+
+echo "Build targets: $ALL_UORS"
 
 # redirect outputs so we can track failures - nysbldo2 does
 # not mail cron job results
