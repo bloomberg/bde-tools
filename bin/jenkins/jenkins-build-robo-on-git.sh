4fd5068ea6eaa229cdb80b98d8f8d491133a8038
Mike Giroux <mgiroux@bloomberg.net>
2015-08-13 10:27:54 -0400

    switch back to unstable

--- bin/jenkins/jenkins-build-robo-on-git.sh
+++ bin/jenkins/jenkins-build-robo-on-git.sh
@@ -68,7 +68,7 @@ fi
 if [[ ! -d data ]]
 then \
     echo Initializing DPKG distro - should be needed only once
-    dpkg-distro-dev init --distribution=robo-trunk .
+    dpkg-distro-dev init --distribution=unstable .
 fi
 
 echo ====================================
