5af71a1997f689005c21c6ed61a68c6fccbe610f
Mike Giroux <mgiroux@bloomberg.net>
2015-08-13 07:58:40 -0400

    add bbl to build targets for nightlies

--- scripts/nightly-run-dev.sh
+++ scripts/nightly-run-dev.sh
@@ -10,7 +10,7 @@ BDE_BB_BRANCH=remotes/origin/dev-integration
 
 BSL_TYPE=internal
 
-CORE_UORS="bsl bdl bde bbe bce bae bte"
+CORE_UORS="bsl bdl bbl bde bbe bce bae bte"
 BB_UORS="bdx bsi zde a_bdema a_bteso a_xercesc e_ipc z_a_bdema bap a_comdb2 a_cdb2 a_cdrdb z_bae a_cdrcache a_iconv"
 
 ALL_UORS="$CORE_UORS $BB_UORS"
