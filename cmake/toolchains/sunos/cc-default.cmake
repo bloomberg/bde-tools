21fa764a316852fbd96305db88f5416d515383e5
Oleg Subbotin <osubbotin@bloomberg.net>
2018-06-22 14:19:22 +0100

    Default toolchain fix for Solaris. (#4)

--- cmake/toolchains/sunos/cc-default.cmake
+++ cmake/toolchains/sunos/cc-default.cmake
@@ -1,7 +1,1 @@
-# Select global build type flags.
-set(CMAKE_CXX_FLAGS "-m${BUILD_BITNESS} -xtarget=generic -xbuiltin=%all"
-    CACHE STRING "Default"        FORCE)
-
-set(CMAKE_C_FLAGS "-m${BUILD_BITNESS} -xtarget=generic"
-    CACHE STRING "Default"        FORCE)
-
+default.cmake
