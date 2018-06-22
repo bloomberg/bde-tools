7c7ec4eb64b4127e0234e0d333c9f0f9db52d80a
Oleg Subbotin <osubbotin@bloomberg.net>
2018-06-22 14:50:23 +0100

    Default toolchain fix for AIX. (#6)

--- cmake/toolchains/aix/xlc-12.1.cmake
+++ cmake/toolchains/aix/xlc-12.1.cmake
@@ -2,41 +2,4 @@ set(root /bb/util/version12-082017/usr/vacpp/bin)
 set(CMAKE_C_COMPILER ${root}/xlc_r)
 set(CMAKE_CXX_COMPILER ${root}/xlC_r)
 
-# Select global build type flags.
-set(CMAKE_CXX_FLAGS "-q${BUILD_BITNESS}"
-    CACHE STRING "Default"        FORCE)
-
-set(CMAKE_CXX_FLAGS_RELEASE         "-O -DNDEBUG"
-    CACHE STRING "Release"        FORCE)
-set(CMAKE_CXX_FLAGS_MINSIZEREL      "-O -DNDEBUG"
-    CACHE STRING "MinSizeRel"     FORCE)
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O -g -DNDEBUG"
-    CACHE STRING "RelWithDebInfo" FORCE)
-set(CMAKE_CXX_FLAGS_DEBUG           "-g"
-    CACHE STRING "Debug"          FORCE)
-
-set(CMAKE_C_FLAGS "-q${BUILD_BITNESS}"
-    CACHE STRING "Default"        FORCE)
-
-set(CMAKE_C_FLAGS_RELEASE           "-O -DNDEBUG"
-    CACHE STRING "Release"        FORCE)
-set(CMAKE_C_FLAGS_MINSIZEREL        "-O -DNDEBUG"
-    CACHE STRING "MinSizeRel"     FORCE)
-set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-O -g -DNDEBUG"
-    CACHE STRING "RelWithDebInfo" FORCE)
-set(CMAKE_C_FLAGS_DEBUG             "-g"
-    CACHE STRING "Debug"          FORCE)
-
-set(CMAKE_C_ARCHIVE_CREATE
-    "<CMAKE_AR> -X${BUILD_BITNESS} cr <TARGET> <LINK_FLAGS> <OBJECTS>")
-set(CMAKE_C_ARCHIVE_APPEND
-    "<CMAKE_AR> -X${BUILD_BITNESS} r <TARGET> <LINK_FLAGS> <OBJECTS>")
-set(CMAKE_C_ARCHIVE_FINISH
-    "<CMAKE_RANLIB> -X${BUILD_BITNESS} <TARGET> <LINK_FLAGS>")
-
-set(CMAKE_CXX_ARCHIVE_CREATE
-    "<CMAKE_AR> -X${BUILD_BITNESS} cr <TARGET> <LINK_FLAGS> <OBJECTS>")
-set(CMAKE_CXX_ARCHIVE_APPEND
-    "<CMAKE_AR> -X${BUILD_BITNESS} r <TARGET> <LINK_FLAGS> <OBJECTS>")
-set(CMAKE_CXX_ARCHIVE_FINISH
-    "<CMAKE_RANLIB> -X${BUILD_BITNESS} <TARGET> <LINK_FLAGS>")
+include("toolchains/aix/default")
