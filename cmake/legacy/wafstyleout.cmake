4c791537e5227487e8154e6fab6eb654a4c51c6f
David Sankel <dsankel@bloomberg.net>
2018-08-06 13:27:19 -0400

    Use 'find_package(PythonInterp)' instead of expecting Python in path. (#39)

--- cmake/legacy/wafstyleout.cmake
+++ cmake/legacy/wafstyleout.cmake
@@ -3,12 +3,14 @@ if(WAFSTYLEOUT_INCLUDED)
 endif()
 set(WAFSTYLEOUT_INCLUDED true)
 
+find_package(PythonInterp REQUIRED)
+
 function(internal_setup_wafstyleout)
     set(absolutePyFilename ${CMAKE_CURRENT_LIST_DIR}/wafstyleout.py)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "python ${absolutePyFilename}")
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "python ${absolutePyFilename}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${PYTHON_EXECUTABLE} ${absolutePyFilename}")
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "${PYTHON_EXECUTABLE} ${absolutePyFilename}")
         # The compiler/linker launchers need a string
-    set_property(GLOBAL PROPERTY BDE_RULE_LAUNCH_TEST python ${absolutePyFilename})
+	set_property(GLOBAL PROPERTY BDE_RULE_LAUNCH_TEST ${PYTHON_EXECUTABLE} ${absolutePyFilename})
         # The test launcher needs a list
 endfunction()
 internal_setup_wafstyleout() # Call immediately for correctness of ${CMAKE_CURRENT_LIST_DIR}
