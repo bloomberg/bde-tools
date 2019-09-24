5cccb638abea22a1aaa61f28e70cdb483db522ed
Mike Giroux <mgiroux@bloomberg.net>
2019-09-24 15:02:49 -0400

    Remove obsolete versioned clang toolchains (#118)

--- cmake/toolchains/linux/clang-6.0.cmake
+++
@@ -1,5 +0,0 @@
-set(root /bb/bde/bbshr/RHEL6-clang-compilers/opt/bb/lib/llvm-6.0/bin)
-set(CMAKE_C_COMPILER ${root}/clang)
-set(CMAKE_CXX_COMPILER ${root}/clang++)
-
-include("toolchains/linux/clang-default")
