4ec06538b090ba7ec13177b22eacb40b0adedd75
Mike Giroux <mgiroux@bloomberg.net>
2022-03-29 10:14:44 -0400

    Use -fno-omit-frame-pointer for Linux opt builds (#221)

--- cmake/toolchains/linux/gcc-default.cmake
+++ cmake/toolchains/linux/gcc-default.cmake
@@ -41,20 +41,20 @@ set(CMAKE_CXX_FLAGS ${DEFAULT_CXX_FLAGS} CACHE STRING "Default" FORCE)
 set(CMAKE_C_FLAGS   ${DEFAULT_C_FLAGS}   CACHE STRING "Default" FORCE)
 
 
-set(CMAKE_CXX_FLAGS_RELEASE         "-O2 -DNDEBUG"
+set(CMAKE_CXX_FLAGS_RELEASE         "-O2 -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "Release"        FORCE)
-set(CMAKE_CXX_FLAGS_MINSIZEREL      "-O2 -DNDEBUG"
+set(CMAKE_CXX_FLAGS_MINSIZEREL      "-O2 -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "MinSizeRel"     FORCE)
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG"
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "RelWithDebInfo" FORCE)
 set(CMAKE_CXX_FLAGS_DEBUG           "-g"
     CACHE STRING "Debug"          FORCE)
 
-set(CMAKE_C_FLAGS_RELEASE           "-O2 -DNDEBUG"
+set(CMAKE_C_FLAGS_RELEASE           "-O2 -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "Release"        FORCE)
-set(CMAKE_C_FLAGS_MINSIZEREL        "-O2 -DNDEBUG"
+set(CMAKE_C_FLAGS_MINSIZEREL        "-O2 -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "MinSizeRel"     FORCE)
-set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-O2 -g -DNDEBUG"
+set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-O2 -g -DNDEBUG -fno-omit-frame-pointer"
     CACHE STRING "RelWithDebInfo" FORCE)
 set(CMAKE_C_FLAGS_DEBUG             "-g"
     CACHE STRING "Debug"          FORCE)
