e81de126c499a24c91e40e7acc6b83f54ef5d80b
Mike Giroux <mgiroux@bloomberg.net>
2022-08-22 08:29:01 -0400

    Handle all bytes in bde input limiter.py drqs170140938 (#242)
    * Switch to perl implementation to handle all bytes
    
    * Enable autoflush

--- bin/bde_input_limiter.py
+++ bin/bde_input_limiter.py
@@ -1,22 +1,56 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env perl
 
-import sys
 
-line_count = 0
-max_count = 5000
+# This used to be a python script.  However, since we want to pass through
+# all bytes unchanged regardless of encoding, it's simpler to reimplement this
+# in perl.
 
-for line in sys.stdin:
-    if line_count > max_count:
-        break
+use warnings;
+use strict;
 
-    sys.stdout.write(line)
-    line_count += 1
+my $max_count = 5000;
 
-if line_count > max_count:
-    print("###############################\n")
-    print("#### OUTPUT LIMIT EXCEEDED ####\n")
-    print("###############################\n")
+$|++;
 
-    # get rid of any remaining input, without causing SIGPIPE upstream
-    for line in sys.stdin:
-        pass
+while (<>) {
+    print;
+
+    last if 0 == --$max_count;
+}
+
+if ($max_count <= 0) {
+     print("###############################\n");
+     print("#### OUTPUT LIMIT EXCEEDED ####\n");
+     print("###############################\n");
+ 
+     # get rid of any remaining input, without causing SIGPIPE upstream
+     while (<>) {
+         # noop
+     }
+}
+
+
+# Old python impl:
+# import sys
+# 
+# line_count = 0
+# max_count = 5000
+# 
+# for line in sys.stdin:
+#     if line_count > max_count:
+#         break
+# 
+#     sys.stdout.write(line)
+#     line_count += 1
+# 
+# if line_count > max_count:
+#     print("###############################\n")
+#     print("#### OUTPUT LIMIT EXCEEDED ####\n")
+#     print("###############################\n")
+# 
+#     # get rid of any remaining input, without causing SIGPIPE upstream
+#     for line in sys.stdin:
+#         pass
+# 
+
+
