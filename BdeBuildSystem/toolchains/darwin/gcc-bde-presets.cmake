b5d25041eaa3f5f758311b20d01c83d5a07ccc1c
osubbotin <osubbotin@bloomberg.net>
2022-10-19 12:13:28 -0400

    Add gcc BBS toolchains on Darwin (#259)

---
+++ BdeBuildSystem/toolchains/darwin/gcc-bde-presets.cmake
@@ -0,0 +1,118 @@
+# Default BDE ufid variables handling.
+#
+# Darwin, gcc
+
+# CMAKE_BUILD_TYPE and CMAKE_POSITION_INDEPENDENT_CODE are
+# not handled by this code and must be passed to cmake directly by
+# BDE or other wrappers (bbcmake) or by plain cmake.
+
+if(BDE_BUILD_TARGET_64)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-m64 "
+           )
+    string(CONCAT DEFAULT_C_FLAGS
+           "${DEFAULT_C_FLAGS} "
+           "-m64 "
+           )
+endif()
+
+if(BDE_BUILD_TARGET_32)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-m32 "
+           )
+    string(CONCAT DEFAULT_C_FLAGS
+           "${DEFAULT_C_FLAGS} "
+           "-m32 "
+           )
+endif()
+
+
+# NOTE: all c++ compilers support exception by default
+# and BDE will by default define BDE_BUILD_TARGET_EXC
+if (BDE_BUILD_TARGET_NO_EXC)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-fno-exceptions "
+           "-DBDE_BUILD_TARGET_NO_EXC "
+           )
+endif()
+
+# Set requested CPP standard
+if (BDE_BUILD_TARGET_CPP03)
+    set(CMAKE_CXX_STANDARD 98)
+elseif(BDE_BUILD_TARGET_CPP11)
+    set(CMAKE_CXX_STANDARD 11)
+elseif(BDE_BUILD_TARGET_CPP14)
+    set(CMAKE_CXX_STANDARD 14)
+elseif(BDE_BUILD_TARGET_CPP17)
+    set(CMAKE_CXX_STANDARD 17)
+elseif(BDE_BUILD_TARGET_CPP20)
+    set(CMAKE_CXX_STANDARD 20)
+elseif(BDE_BUILD_TARGET_CPP23)
+    set(CMAKE_CXX_STANDARD 23)
+else()
+    # c++17 is default on Darwing when nothing is set
+    set(CMAKE_CXX_STANDARD 17)
+endif()
+
+# Disable GNU c++ extensions.
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# Sanitizers
+if(BDE_BUILD_TARGET_ASAN)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-fsanitize=address "
+           )
+    string(CONCAT DEFAULT_C_FLAGS
+           "${DEFAULT_C_FLAGS} "
+           "-fsanitize=address "
+           )
+    string(CONCAT DEFAULT_EXE_LINKER_FLAGS
+           "${DEFAULT_EXE_LINKER_FLAGS} "
+           "-fsanitize=address "
+           "-static-libasan "
+           )
+endif()
+
+if(BDE_BUILD_TARGET_MSAN)
+    message(FATAL_ERROR "Memory sanitizer is not available for gcc.")
+endif()
+
+if(BDE_BUILD_TARGET_TSAN)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-fsanitize=thread "
+           )
+    string(CONCAT DEFAULT_C_FLAGS
+           "${DEFAULT_C_FLAGS} "
+           "-fsanitize=thread "
+           )
+    string(CONCAT DEFAULT_EXE_LINKER_FLAGS
+           "${DEFAULT_EXE_LINKER_FLAGS} "
+           "-fsanitize=thread "
+           "-static-libtsan "
+           )
+endif()
+
+if(BDE_BUILD_TARGET_UBSAN)
+    string(CONCAT DEFAULT_CXX_FLAGS
+           "${DEFAULT_CXX_FLAGS} "
+           "-fsanitize=undefined "
+           )
+    string(CONCAT DEFAULT_C_FLAGS
+           "${DEFAULT_C_FLAGS} "
+           "-fsanitize=undefined "
+           )
+    string(CONCAT DEFAULT_EXE_LINKER_FLAGS
+           "${DEFAULT_EXE_LINKER_FLAGS} "
+           "-fsanitize=undefined "
+           "-static-libubsan "
+           )
+endif()
+
+if(BDE_BUILD_TARGET_FUZZ)
+    message(FATAL_ERROR "Fuzzer is not available for gcc.")
+endif()
---
+++ BdeBuildSystem/toolchains/darwin/gcc-default.cmake
@@ -0,0 +1,79 @@
+# Default compiler-less toolchains for BBS build system.
+# The actual compiler for this toolchain is passed via CXX and CC 
+# environment variables.
+#
+# Darwin, gcc
+
+set(DEFAULT_CXX_FLAGS "$ENV{CXXFLAGS}")
+set(DEFAULT_C_FLAGS "$ENV{CFLAGS}")
+
+set(CXX_WARNINGS
+    "-Waddress "
+    "-Wall "
+    "-Wcast-align "
+    "-Wcast-qual "
+    "-Wconversion "
+    "-Wextra "
+    "-Wformat "
+    "-Wformat-security "
+    "-Wformat-y2k "
+    "-Winit-self "
+    "-Wlarger-than-100000 "
+    "-Wlogical-op "
+    "-Woverflow "
+    "-Wpacked "
+    "-Wparentheses "
+    "-Wpointer-arith "
+    "-Wsign-compare "
+    "-Wstrict-overflow=1 "
+    "-Wtype-limits "
+    "-Wvla "
+    "-Wvolatile-register-var "
+    "-Wwrite-strings "
+    "-Wno-char-subscripts "
+    "-Wno-long-long "
+    "-Wno-sign-conversion "
+    "-Wno-unknown-pragmas "
+    "-Wno-unused-value "
+    )
+
+string(CONCAT DEFAULT_CXX_FLAGS
+       "${DEFAULT_CXX_FLAGS} "
+       "-fdiagnostics-show-option "
+       "-fno-strict-aliasing "
+       "-fno-omit-frame-pointer "
+       ${CXX_WARNINGS}
+      )
+
+string(CONCAT DEFAULT_C_FLAGS
+       "${DEFAULT_C_FLAGS} "
+       "-fdiagnostics-show-option "
+       "-fno-strict-aliasing "
+      )
+
+# Include BDE ufid presets
+include("${CMAKE_CURRENT_LIST_DIR}/gcc-bde-presets.cmake")
+
+# After picking various ufid flags, make them default.
+set(CMAKE_CXX_FLAGS        ${DEFAULT_CXX_FLAGS} CACHE STRING "Default" FORCE)
+set(CMAKE_C_FLAGS          ${DEFAULT_C_FLAGS}   CACHE STRING "Default" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS ${DEFAULT_EXE_LINKER_FLAGS} CACHE STRING "Default" FORCE)
+
+set(CMAKE_CXX_FLAGS_RELEASE         "-O2 -DNDEBUG"
+    CACHE STRING "Release"        FORCE)
+set(CMAKE_CXX_FLAGS_MINSIZEREL      "-O2 -DNDEBUG"
+    CACHE STRING "MinSizeRel"     FORCE)
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG"
+    CACHE STRING "RelWithDebInfo" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG           "-g"
+    CACHE STRING "Debug"          FORCE)
+
+set(CMAKE_C_FLAGS_RELEASE           "-O2 -DNDEBUG"
+    CACHE STRING "Release"        FORCE)
+set(CMAKE_C_FLAGS_MINSIZEREL        "-O2 -DNDEBUG"
+    CACHE STRING "MinSizeRel"     FORCE)
+set(CMAKE_C_FLAGS_RELWITHDEBINFO    "-O2 -g -DNDEBUG"
+    CACHE STRING "RelWithDebInfo" FORCE)
+set(CMAKE_C_FLAGS_DEBUG             "-g"
+    CACHE STRING "Debug"          FORCE)
+
