b898dc3e249c891226dd6e6143b834684cccbf36
Oleg Subbotin <osubbotin@bloomberg.net>
2023-01-17 18:13:16 -0500

    Do not de-duplicate options from dpkg toolchains (#278)

--- BdeBuildSystem/toolchains/dpkg/BdeToolchain32.cmake
+++ BdeBuildSystem/toolchains/dpkg/BdeToolchain32.cmake
@@ -9,24 +9,13 @@ if(NOT DEFINED DISTRIBUTION_REFROOT)
     endif()
 endif()
 
-function(remove_duplicates input_str output_str)
-    set(input_list ${${input_str}})
-    separate_arguments(input_list)
-    list(REMOVE_DUPLICATES input_list)
-    string (REGEX REPLACE "([^\\]|^);" "\\1 " _tmp_str "${input_list}")
-    string (REGEX REPLACE "[\\](.)" "\\1" _tmp_str "${_tmp_str}") #fixes escapes
-    set (${output_str} "${_tmp_str}" PARENT_SCOPE)
-endfunction()
-
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
     include("${DISTRIBUTION_REFROOT}/opt/bb/share/plink/BBToolchain32.cmake")
 
     string(APPEND CMAKE_C_FLAGS   " -fno-strict-aliasing")
-    remove_duplicates(CMAKE_C_FLAGS _tmp_C_FLAGS)
     set(CMAKE_C_FLAGS ${_tmp_C_FLAGS} CACHE STRING "Bloomberg ABI C flags." FORCE)
 
     string(APPEND CMAKE_CXX_FLAGS " -fno-strict-aliasing")
-    remove_duplicates(CMAKE_CXX_FLAGS _tmp_CXX_FLAGS)
     set(CMAKE_CXX_FLAGS ${_tmp_CXX_FLAGS} CACHE STRING "Bloomberg ABI C++ flags." FORCE)
 endif()
 
@@ -34,11 +23,9 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL "AIX")
     include("${DISTRIBUTION_REFROOT}/opt/bb/share/plink/BBToolchain32.cmake")
 
     string(APPEND CMAKE_C_FLAGS   " -qalias=noansi")
-    remove_duplicates(CMAKE_C_FLAGS _tmp_C_FLAGS)
     set(CMAKE_C_FLAGS ${_tmp_C_FLAGS} CACHE STRING "Bloomberg ABI C flags." FORCE)
 
     string(APPEND CMAKE_CXX_FLAGS " -qalias=noansi")
-    remove_duplicates(CMAKE_CXX_FLAGS _tmp_CXX_FLAGS)
     set(CMAKE_CXX_FLAGS ${_tmp_CXX_FLAGS} CACHE STRING "Bloomberg ABI C++ flags." FORCE)
 endif()
 
@@ -46,11 +33,9 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL "SunOS")
     include("${DISTRIBUTION_REFROOT}/opt/bb/share/plink/BBToolchain32.cmake")
 
     string(APPEND CMAKE_C_FLAGS " -xthreadvar=dynamic")
-    remove_duplicates(CMAKE_C_FLAGS _tmp_C_FLAGS)
     set(CMAKE_C_FLAGS ${_tmp_C_FLAGS} CACHE STRING "Bloomberg ABI C flags." FORCE)
 
     string(APPEND CMAKE_CXX_FLAGS " -xthreadvar=dynamic")
-    remove_duplicates(CMAKE_CXX_FLAGS _tmp_CXX_FLAGS)
     set(CMAKE_CXX_FLAGS ${_tmp_CXX_FLAGS} CACHE STRING "Bloomberg ABI C++ flags." FORCE)
 endif()
 
