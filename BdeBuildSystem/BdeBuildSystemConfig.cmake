64c75c8969a7ab94bf2ed7949f5a17fb1b72095b
Oleg Subbotin <osubbotin@bloomberg.net>
2025-04-25 12:32:42 -0400

    Make include guard in directory scope (#385)

--- BdeBuildSystem/BdeBuildSystemConfig.cmake
+++ BdeBuildSystem/BdeBuildSystemConfig.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.15)
 
-include_guard()
+include_guard(DIRECTORY)
 
 # Sub-modules are listed here in dependency order
 include(${CMAKE_CURRENT_LIST_DIR}/BdeBuildSystemUtils.cmake)
